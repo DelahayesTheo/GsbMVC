5d3951533205ec8a0e9f147ca25540c80e810d8f
DelahayesTheo <delahayest@gmail.com>
2017-02-27 10:36:38 +0100

    zafeùpjjojfzePO%

--- src/FraisApiBundle/Controller/FraisforfaitRestController.php
+++ src/FraisApiBundle/Controller/FraisforfaitRestController.php
@@ -3,7 +3,44 @@
 namespace FraisApiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+require_once("include/fct.inc.php");
+require_once ("include/class.pdogsb.inc.php");
 
+use Symfony\Component\HttpFoundation\Request;
+use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use PdoGsb;
 class FraisforfaitRestController extends Controller
 {
+    public function getLesFraisForfait($idVisiteur, $mois)
+    {
+        $pdo = PdoGsb::getPdoGsb();
+        $lesfrais = $pdo->getLesFraisForfait($idVisiteur, $mois);
+
+        if(!$lesfrais)
+        {
+            throw new NotFoundHttpException('Mois non disponibles [idVisiteur='.$idVisiteur.']');
+        }
+
+        return new JsonResponse($lesfrais);
+    }
+
+    public function getLesIdFrais()
+    {
+        $pdo = PdoGsb::getPdoGsb();
+        $lesid = $pdo->getLesIdFrais();
+
+        if (!lesid) {
+            throw new NotFoundHttpException('Id non disponibles');
+        }
+
+        return new JsonResponse($lesid);
+    }
+
+    public function majFraisForfait($idVisiteur, $mois, $lesFrais)
+    {
+        $pdo = PdoGsb::getPdoGsb();
+    }
 }
